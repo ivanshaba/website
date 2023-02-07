eb75c7ff182d9197a0610099c4ff53c5b11b834e
user <user@users-MacBook-Pro.local>
2023-02-07 16:01:51 +0300

    changes

--- index.js
+++ index.js
@@ -8,6 +8,28 @@
 //     Form.classList.add('form__container__active');
 // });
 
+const mainMenu = document.querySelector(".mainMenu");
+const closeMenu = document.querySelector(".closeMenu");
+const openMenu = document.querySelector(".openMenu");
+const Transition = document.querySelector(".transition");
+const Animae = document.querySelector(".animae");
+
+openMenu.addEventListener("click", show)
+closeMenu.addEventListener("click", close);
+function show() {
+    mainMenu.style.display = "flex";
+    mainMenu.style.top = "0";
+    mainMenu.style.background = "#000";
+    mainMenu.style.transition = " all 1s ease-in-out";
+}
+
+function close() {
+    mainMenu.style.top = "-200%";
+    mainMenu.style.background = "transparent";
+    mainMenu.style.transition = "all 1s ease-out";
+    mainMenu.style.zIndex = "0";
+    // mainMenu.style.display = "none";
+}
 
 
 // var mysql = require("mysql2");
@@ -18,27 +40,43 @@
 //   database: "music_library"
 // });
 
-// connection.connect();
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
 
-// var playlist = [];
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// function addSong(title, artist, fileLocation) {
-//   var song = {
-//     title: title,
-//     artist: artist,
-//     fileLocation: fileLocation
-//   };
+app.post("/form", (req, res) => {
+  const { name, email, message } = req.body;
+  if (!name || !email || !message) {
+    return res.status(400).send("Bad Request");
+  }
 
-//   connection.query("INSERT INTO songs (title, artist, file_location) VALUES (?, ?, ?)", [title, artist, fileLocation], function(error, results, fields) {
-//     if (error) throw error;
-//     console.log("Song added to the database");
-//   });
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "your-gmail-email@gmail.com",
+      pass: "your-gmail-password",
+    },
+  });
 
-//   playlist.push(song);
-// }
+  const mailOptions = {
+    from: email,
+    to: "your-gmail-email@gmail.com",
+    subject: "Form Submission",
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  };
 
-// connection.query("SELECT * FROM songs", function(error, results, fields) {
-//   if (error) throw error;
-//   playlist = results;
-//   console.log("Playlist loaded from the database");
-// });
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).send(error);
+    }
+    return res.status(200).send("Form data submitted successfully");
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server started on http://localhost:3000");
+});
