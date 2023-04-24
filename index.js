9754734d33439c456011b677edfb1117fe90112a
ivan shaba <user@users-MacBook-Pro.local>
2023-04-24 05:37:57 +0300

    deploy

--- index.js
+++ index.js
@@ -22,46 +22,90 @@ function close() {
 
 
 
+const {google} = require('googleapis');
+const OAuth2 = google.auth.OAuth2;
 
+const oauth2Client = new OAuth2(
+  'YOUR_CLIENT_ID',
+  'YOUR_CLIENT_SECRET',
+  'YOUR_REDIRECT_URL'
+);
 
+oauth2Client.setCredentials({
+  access_token: 'YOUR_ACCESS_TOKEN',
+  refresh_token: 'YOUR_REFRESH_TOKEN'
+});
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const nodemailer = require("nodemailer");
+const gmail = google.gmail({version: 'v1', auth: oauth2Client});
 
-const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+function sendMail(formData) {
+  const message = [
+    'Content-Type: text/html; charset=utf-8\r\n',
+    'To: YOUR_EMAIL_ADDRESS\r\n',
+    'Subject: New form submission\r\n',
+    '\r\n',
+    `<p>Name: ${formData.name}</p><p>Email: ${formData.email}</p><p>Message: ${formData.message}</p>`
+  ].join('');
 
-app.post("/form", (req, res) => {
-  const { name, email, message } = req.body;
-  if (!name || !email || !message) {
-    return res.status(400).send("Bad Request");
-  }
-
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "your-gmail-email@gmail.com",
-      pass: "your-gmail-password",
-    },
-  });
-
-  const mailOptions = {
-    from: email,
-    to: "your-gmail-email@gmail.com",
-    subject: "Form Submission",
-    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return res.status(500).send(error);
+  const encodedMessage = Buffer.from(message).toString('base64');
+  const request = gmail.users.messages.send({
+    userId: 'me',
+    requestBody: {
+      raw: encodedMessage
     }
-    return res.status(200).send("Form data submitted successfully");
   });
-});
 
-app.listen(3000, () => {
-  console.log("Server started on http://localhost:3000");
-});
+  request.execute((err, res) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(res);
+    }
+  });
+}
+
+
+
+
+
+
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const nodemailer = require("nodemailer");
+
+// const app = express();
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+// app.post("/form", (req, res) => {
+//   const { name, email, message } = req.body;
+//   if (!name || !email || !message) {
+//     return res.status(400).send("Bad Request");
+//   }
+
+//   const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: "your-gmail-email@gmail.com",
+//       pass: "your-gmail-password",
+//     },
+//   });
+
+//   const mailOptions = {
+//     from: email,
+//     to: "your-gmail-email@gmail.com",
+//     subject: "Form Submission",
+//     text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+//   };
+
+//   transporter.sendMail(mailOptions, (error, info) => {
+//     if (error) {
+//       return res.status(500).send(error);
+//     }
+//     return res.status(200).send("Form data submitted successfully");
+//   });
+// });
+
+// app.listen(3000, () => {
+//   console.log("Server started on http://localhost:3000");
+// });
